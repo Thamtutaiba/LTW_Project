e3ce45847095be7a77dbadc0e9dcc494d188c99a
Thamtutaiba <conanahungdinhvan123@gmail.com>
2025-07-23 22:32:14 +0700

    upload code

--- add_address_id_to_orders.sql
+++
@@ -1,6 +0,0 @@
-
-ALTER TABLE orders ADD COLUMN address_id INT NULL AFTER user_id;
-ALTER TABLE orders ADD CONSTRAINT orders_address_fk 
-FOREIGN KEY (address_id) REFERENCES user_addresses(id) ON DELETE SET NULL;
-CREATE INDEX idx_orders_address_id ON orders(address_id);
-
